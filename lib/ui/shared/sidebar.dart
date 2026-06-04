4c9be9a617901d1e925444ce592f0fb8ff7aa4ea
Yuleisi Barrera Castro <s22030157@itsch.edu.mx>
2026-06-03 22:43:38 -0600

    categoria,plagas,producto terminados

--- lib/ui/shared/sidebar.dart
+++ lib/ui/shared/sidebar.dart
@@ -32,7 +32,7 @@ class Sidebar extends StatelessWidget {
           //Opciones
           MenuItem(text: 'Inicio', icon: Icons.home_outlined, onPressed: ()=> navigateTo(Flurorouter.dashboardRoute)),
           MenuItem(text: 'Nosotros', icon: Icons.groups_outlined, onPressed: ()=> navigateTo(Flurorouter.blackRoute)),
-          MenuItem(text: 'Destacados', icon: Icons.star_outline, onPressed: ()=> navigateTo(Flurorouter.analyticRoute)),
+          MenuItem(text: 'Plagas', icon: Icons.bug_report_outlined, onPressed: ()=> navigateTo(Flurorouter.analyticRoute)),
           MenuItem(text: 'Categorías', icon: Icons.grid_view_outlined, onPressed: ()=> navigateTo(Flurorouter.categoriesRoute)),
           MenuItem(text: 'Productos', icon: Icons.inventory_2_outlined, onPressed: ()=> navigateTo(Flurorouter.productsRoute)),
           MenuItem(text: 'Populares', icon: Icons.local_fire_department_outlined, onPressed: ()=> navigateTo(Flurorouter.discountRoute)),
